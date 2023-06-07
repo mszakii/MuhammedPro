5e59133cf951c5c16c5ffdbeba5db6fbf7edca62
smy <smy.family77@gmail.com>
2023-06-07 09:49:03 +0300

    fourth update cards and dynamic content

---
+++ js/master.js
@@ -0,0 +1,23 @@
+fetch("js/main.json")
+  .then((data) => data.json())
+  .then((data) => {
+    for (const key in data) {
+      let pro = data[key];
+      console.log(pro);
+      let div = document.createElement("div");
+      div.className = "card";
+      div.innerHTML = `
+        <div class="photo">
+          <img src="${pro.photo}" alt="project photo" />
+        </div>
+        <div class="caption">
+          <div class="title">${pro.name}</div>
+          <div class="des">${pro.des}</div>
+          <div class="btn" onclick="location.href='${pro.link}'">
+          ${pro.btn_name}
+          </div>
+        </div>
+      `;
+      workE.appendChild(div);
+    }
+  });
